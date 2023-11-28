121882c61a45a97d59c4363b4a4d3f22f0c73273
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-11-28 13:41:38 -0800

    p

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last = -(abs(number)%10)
+    last = -(abs(number) % 10)
 else:
     last = number % 10
 if last < 6 and last != 0:
