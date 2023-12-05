1101f9080f7a32b4764e657591580e3b1a9c1867
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-05 10:59:41 -0800

    p

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         operator = sys.argv[2]
         b = int(sys.argv[3])
-        if args[2] == '+':
+        if operator == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif args[2] == '-':
+        elif operator == '-':
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif args[2] == '/':
+        elif operator == '/':
             print("{} / {} = {}".format(a, b, div(a, b)))
-        elif args[2] == '*':
+        elif operator == '*':
             print("{} * {} = {}".format(a, b, mul(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
