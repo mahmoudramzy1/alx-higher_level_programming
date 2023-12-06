b19a230cdbc0e4a3d4f2640b90cb56ba63666f41
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-06 13:38:35 -0800

    p

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,7 @@ def roman_to_int(roman_string):
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         n = 0
         for i in roman_string:
-            for x, y in d.items():
-                if i == x:
-                    n += y
+             y = d[i]
+            n += y if n < y * 5 else -n
         return n
     return 0
