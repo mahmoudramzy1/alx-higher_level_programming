cdc0ec26fa042b55a0df17f6b2ff4f70f1bf571b
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-06 13:23:42 -0800

    p

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) == str:
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         n = 0
         for i in roman_string:
             for x, y in d.items():
