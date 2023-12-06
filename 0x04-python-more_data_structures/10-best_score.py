213d3033aa15dcfeb75daf66733094de9ab24e65
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-06 12:52:39 -0800

    p

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for i, n in dictionary.items():
-        
+    if a_dictionary:
+        n = list(a_dictionary.values())[0]
+        for i, m in a_dictionary.items():
+            if m > n:
+                n = m
+                k = i
+        return k
+    return None
