ceb68a41936ae6b678e653839d1b926c896ba250
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-06 13:17:54 -0800

    p

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if type(roman_string) == str:
+        n = 0
+        for i in roman_string:
+            for x, y in d.items():
+                if i == x:
+                    n += y
+        return n
+    return 0
