8c30d63ced6bb0dbdf441119a535641535253d6a
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-06 08:57:44 -0800

    p

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             n.append(replace)
-        else: 
+        else:
             n.append(i)
     return n
