9db5b0567bd1b44726925a880c1d20c769d2b739
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-19 10:33:10 -0800

    p

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+class Square:
+    def __init__(self, size):
+        """
+        args:
+            size: length of the side of the square
+            """
+        self.__size = size
