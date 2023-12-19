c8c0cdd4e3ff6230936d2d8db06065bdbe834484
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2023-12-19 10:55:16 -0800

    p

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+class Square:
+    def __init__(self, size = 0):
+        """
+        Args:
+            size: length of the side of the square
+        Raise:
+            TypeError: if size is not an integer
+            ValueError: if size is less than zero
+            """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
