d2f0e83127b3ca13e77c69bd389abe834d094017
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 15:04:27 -0800

    p

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ def save_to_json_file(my_obj, filename):
     """write my_obj in file with json repr"""
 
     with open(filename, 'w', encoding='utf-8') as f:
-        return json.dumps(obj, f)
+        return json.dump(obj, f)
