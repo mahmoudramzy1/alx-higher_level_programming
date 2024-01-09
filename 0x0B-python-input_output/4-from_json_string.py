38432dc38dd665d7557b306da905960a8a34abf7
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 14:49:49 -0800

    p

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define function that convert json string to python datastructure"""
+
+
+def from_json_string(my_str):
+    """Converts from json string to python datastructure"""
+    return json.loads(my_str)
