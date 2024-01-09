81b4d71f901e9bb7f2c4a1378093f7334fe448be
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 15:22:20 -0800

    p

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-"""
+"""Defines function that loads from json file"""
+
+
+def load_from_json_file(filename):
+    """loads from json file"""
+    with open(filename, encoding='utf-8') as a:
+        json.load(a)
