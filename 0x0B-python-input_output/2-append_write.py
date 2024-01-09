87cf88358230940fec47e3b7aaf5c505a285f0dd
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 14:41:13 -0800

    p

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """append the file with text"""
     with open(filename, 'a', encoding='utf-8') as f:
-        return f.append(text)
+        return f.write(text)
