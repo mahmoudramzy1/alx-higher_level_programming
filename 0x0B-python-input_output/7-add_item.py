af65b771d894cee20f1e1bae3f39ba9e5246d4d9
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 16:00:12 -0800

    p

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    old_data = load_from_json_file('add_item.json')
+    json_list = load_from_json_file('add_item.json')
 except Exception:
-    old_data = []
+    json_list = []
 
 for item in argv[1:]:
     json_list.append(item)
