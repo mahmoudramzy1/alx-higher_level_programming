2d62e947fb71e5142762cfa501c1f725f19d184a
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-09 16:03:51 -0800

    p

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     json_list = load_from_json_file('add_item.json')
-except  (ValueError, FileNotFoundError):
+except(ValueError, FileNotFoundError):
     json_list = []
 
 for item in argv[1:]:
