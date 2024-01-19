6ae215c9a381ce86c25664ec258722f9d0a70293
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-01-19 14:24:35 -0800

    p

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """this is the circle module"""
-
+from json import dumps, loads
 
 class Base:
     """this is the base class"""
@@ -13,3 +13,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
