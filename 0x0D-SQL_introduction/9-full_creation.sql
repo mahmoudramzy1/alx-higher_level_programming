0add6447fa73bc4a4377c364f6c045e5ce3ba8fd
git <mahmoud.ahmed.ramzy@gmail.com>
2024-02-13 09:43:19 -0800

    p

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- creates second table and insert values in it.
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "Goerge", 8);
