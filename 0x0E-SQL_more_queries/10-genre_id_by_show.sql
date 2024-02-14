cd906e94247e35d82339848a9d858a4ae02493c7
git <mahmoud.ahmed.ramzy@gmail.com>
2024-02-14 13:40:11 -0800

    p

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- list movies.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_shows_genres ON tv_show.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
