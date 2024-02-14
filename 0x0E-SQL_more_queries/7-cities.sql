fed68d7fffb89d8d3f45a489a640db3a4623afd3
git <mahmoud.ahmed.ramzy@gmail.com>
2024-02-14 13:09:16 -0800

    p

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id REFERENCE hbtn_0d_usa.states(id)),
+	FOREIGN KEY(state_id) REFERENCE hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL);
