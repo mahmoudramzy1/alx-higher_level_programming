dd30ac65620a87f4695e92476922b5a4ff3f0aed
git <mahmoud.ahmed.ramzy@gmail.com>
2024-02-14 13:40:53 -0800

    p

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- join.
-SELECT cities.id, cities.name, states.name
-FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 JOIN STATES ON cities.states_id=states.id
 ORDER BY cities.id;
