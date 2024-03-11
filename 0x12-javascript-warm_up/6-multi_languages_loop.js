e7a8034e5ee7995e53ecb651e1f8f443c3d0e02f
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-11 16:56:51 -0700

    p

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const s = [`C is fun`, `Python is cool`, `JavaScript is amazing`];
-for (let S of s) {
+const s = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const S of s) {
   console.log(S);
 }
