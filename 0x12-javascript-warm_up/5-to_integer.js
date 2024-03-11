675cc59fe806d9a1738982fc7f4365f05e14bdb7
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-11 16:47:54 -0700

    p

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let x = process.argv[2];
 x = Number(x);
-if (typeof(x) != Number){
+if (typeof x !== 'number') {
   console.log('Not a number');
 } else {
   console.log(`My number: ${x}`);
