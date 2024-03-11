480418eaf1c64955ee5c5e8bdaf864c9ca81bfb0
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-11 16:25:42 -0700

    p

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-let x = process.argv[2];
-let y = process.argv[3];
-if (x === undefined)
-	console.log('No argument');
-else
-	if (y === undefined)
-		console.log('Argument found');
-	else
-		console.log('Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
