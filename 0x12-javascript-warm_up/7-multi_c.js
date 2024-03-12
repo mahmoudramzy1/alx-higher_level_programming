00eebb799bc028c85cce547588c2e99680700e75
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-03-12 07:28:12 -0700

    p

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let x = Number(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  while (x !== 0) {
+  while (x > 0) {
     console.log(y);
     x--;
   }
