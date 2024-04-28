d24fa515dad97a7b1f213800a51289318a309878
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-04-28 09:28:21 -0700

    p

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,13 +6,13 @@ from sqlalchemy import Integer, String, create_engine, ForeignKey, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 Base = declarative_base()
+
 
 class State(Base):
     """state class"""
 
-    
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
