6f66aa4dcf8400ff3ae8bc48da8859f58cc4a881
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-05-11 06:37:30 -0700

    p

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,12 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({"email": email}).encode("ascii")
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    html = response.read()
-    print(html.decode("utf-8"))
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
