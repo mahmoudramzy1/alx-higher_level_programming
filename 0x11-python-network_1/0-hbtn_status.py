3fa1eb3d264c4503a249f59342b97c1c1cb9ae37
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-05-10 21:00:40 -0700

    p

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 print('Body response:')
-print('\t- type: ', type(html))
-print('\t- content: ', html)
-print('\t- utf8 ', html.decode('utf-8'))
+print('\t- type:', type(html))
+print('\t- content:', html)
+print('\t- utf8 content:', html.decode('utf-8'))
