d53c7342723ff2b792249f2f01b4172379b69df1
root <mahmoud.ahmed.ramzy@gmail.com>
2024-06-04 11:31:54 -0700

    p

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err);
-        process.exit(1);
-    }
-    console.log(data);
+  if (err) {
+    console.error('Error reading file:', err);
+    process.exit(1);
+  }
+  console.log(data);
 });
