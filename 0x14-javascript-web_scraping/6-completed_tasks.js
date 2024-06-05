a5ef41e13bcb14637cac835bbdb328ffbc1b1f23
root <mahmoud.ahmed.ramzy@gmail.com>
2024-06-04 17:07:56 -0700

    p

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const url = process.argv[2];
-let mydict = {}
-let count = 0
-let id = null;
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
-  list = JSON.parse(body)
-  for (let i in list) {
-    const item = list[i];
-    if (id !== null && id !== item.userId) {
-      mydict[id] = count;
-      count = 0;
-    }
-    id = item.userId 
-    if (item.completed === true)
-      count++;
-  }
-  if (id !== null) {
-    mydict[id] = count;
-  }
-  console.log(mydict);
 });
